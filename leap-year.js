4fae99b4f4eaa9bb9f30e1b5da9df38b3a648dac
Nurul2022 <nurulbdpersonal@gmail.com>
2022-02-09 09:09:16 +0600

    mb final

--- leap-year.js
+++ leap-year.js
@@ -7,10 +7,4 @@ function isLeapYear(year) {
     }
 }
 
-const myYear = 2100;
-const isMyYearLeapYear = isLeapYear(myYear);
-console.log('Is my year leap year', isMyYearLeapYear);
 
-const yourYear = 2224;
-const isYourYearLeapYear = isLeapYear(yourYear);
-console.log('is your year leap year', isYourYearLeapYear);
